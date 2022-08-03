97cb3a60279d4b821ee252467a9590b7edff65c1
mruttan1 <mruttan1@myseneca.ca>
2022-08-03 09:55:37 -0400

    initial commit from IntelliJ

---
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Message;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MessageController {
+
+
+    @GetMapping("/message")
+    Message send() {
+        return new Message("first message");
+    }
+
+    @PostMapping("/message")
+    Message echo (@RequestBody Message message) {
+        return message;
+    }
+}
